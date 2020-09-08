a9c364e242fa1d6bb8bc1f83b694f370443bd6e0
Archu <Archu@Archana>
2020-09-08 09:07:13 -0400

    ComparingStrings

---
+++ JavaExample/src/practice/CompareStrings.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class CompareStrings {
+
+	public static void main(String[] args) {
+		
+		String str = "Java Selenium";
+		String str1 = "Java Selenium";
+		
+		if(str.equals(str1)) {
+			System.out.println("Equal");		
+			}
+		else {
+		System.out.println("Not Equal");
+	}
+	}
+}

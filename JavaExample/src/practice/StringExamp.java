da748a9a1f6e2c6ea5747ccefe4abec87566f446
Archu <Archu@Archana>
2020-09-08 09:23:30 -0400

    Specified Sequence of char values

---
+++ JavaExample/src/practice/StringExamp.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class StringExamp {
+	
+	public static void main(String[] args) {
+		
+		String str = "Specified sequence of a char values";
+		String str1 = "char";
+		
+		System.out.println("Original String : " + str);
+		System.out.println("Specified String : " + str1);
+		System.out.println(str.contains(str1));
+	}
+
+}

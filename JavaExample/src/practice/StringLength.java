3a2777b3d2f8c75bb00d6a4e5e76f019a4a351eb
Archu <Archu@Archana>
2020-09-08 09:27:42 -0400

    Length oof a given string

---
+++ JavaExample/src/practice/StringLength.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class StringLength {
+	
+	public static void main(String[] args) {
+		
+		String str = "Java Class";
+		
+		String str1 = "selineum class";
+		
+		System.out.println("Length of a String : " +str.length());
+		System.out.println("Length of a String : " +str1.length());
+	}
+
+}

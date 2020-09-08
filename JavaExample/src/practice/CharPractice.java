2d8344528adce84493140cab0b8bca1effd8e270
Archu <Archu@Archana>
2020-09-08 08:50:38 -0400

    Character at the given index within the string

---
+++ JavaExample/src/practice/CharPractice.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class CharPractice {
+	
+	public static void main(String[] args) {
+		
+		String str = "JavaSelenium";
+		
+		System.out.println("Original Sentence: " + str);
+		
+		int i = str.charAt(0);
+		int i1 = str.charAt(9);
+		
+		System.out.println("The character at index 0 is :" + (char)i);
+		System.out.println("The character at index 5 is :" + (char)i1);
+		
+		
+	}
+
+}

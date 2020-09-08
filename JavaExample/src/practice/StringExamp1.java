9aca9722af446389afa7a07787ff57acc4ed51c6
Archu <Archu@Archana>
2020-09-08 09:34:02 -0400

    String  converting Lower and Upper case

---
+++ JavaExample/src/practice/StringExamp1.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class StringExamp1 {
+	
+	public static void main(String[] args) {
+		
+		String str = "Learning java selenium";
+		String strUpperCase = str.toUpperCase();
+		
+		System.out.println("Original Sentence : " + str);
+		System.out.println("UpperCase Converted Sentence : " + strUpperCase);
+	}
+
+}
---
+++ JavaExample/src/practice/StringExamp2.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class StringExamp2 {
+public static void main(String[] args) {
+		
+		String str = "LEARNING JAVA SELENIUM";
+		String strLowerCase = str.toLowerCase();
+		
+		System.out.println("Original Sentence : " + str);
+		System.out.println("UpperCase Converted Sentence : " + strLowerCase);
+	}
+
+}
